391ef9c3b1d917876163819d775b96bf4447346a
Javier Rey <javirey@gmail.com>
2013-04-17 22:20:20 -0300

    Se agrega palindromo pero salteo max que no se como hacerlo

--- obligatorio1/grupo12.pl
+++ obligatorio1/grupo12.pl
@@ -5,12 +5,11 @@
     Ej corregido: nth([a,b,c], 2, b)
 */
 
-/*nth([], 0, _).*/
 nth([X|_], 1, X).
 nth([_|Xs], Pos, X) :-
         Pos > 0,
-        Next is Pos - 1,
-        nth(Xs, Next, X).
+        Prev is Pos - 1,
+        nth(Xs, Prev, X).
 
 /*
     sublist_n(?L,+N,?Sub)
@@ -20,14 +19,14 @@ nth([_|Xs], Pos, X) :-
 */
 
 sublist_n(_, 0, []).
-sublist_n([_|Xs], N, Y) :- 
+sublist_n([_|Xs], N, Y) :-
         sublist_n(Xs, N, Y).
-sublist_n([X|Xs], N, [X|Ys]) :- 
+sublist_n([X|Xs], N, [X|Ys]) :-
         Z is N-1,
         prefix_n(Xs, Z, Ys).
 
 prefix_n(_, 0, []).
-prefix_n([X|Xs], N, [X|Ys]) :- 
+prefix_n([X|Xs], N, [X|Ys]) :-
         Z is N-1,
         prefix_n(Xs, Z, Ys).
 
@@ -39,6 +38,14 @@ prefix_n([X|Xs], N, [X|Ys]) :-
 */
 
 max([X], X, []).
-max([X|Xs], X, _) :- 
-        max(Xs, Z, _),
-        X >= Z. 
+%todo
+
+/*
+    palindromo(+L)
+    La lista L es un palindromo.
+    Ej. palindromo([a,b,c,b,a])
+*/
+
+palindromo(L) :-
+        reverse(L, L).
+
--- obligatorio1/tests.pl
+++ obligatorio1/tests.pl
@@ -1,15 +1,13 @@
 :- begin_tests(lists).
 :- use_module(library(lists)).
 
-%% nth - Determinista
+% nth
 
 test(nth_1) :-
         nth([a,b,c], 2, b), !.
 
 test(nth_2, [fail]) :-
         nth([a,b,c], 2, a), !.
-
-%% nth - No Determinista
 
 test(nth_3) :-
         findall(X, nth([a,b,c], 2, X), Xs),
@@ -23,7 +21,7 @@ test(nth_5) :-
         nth(X, 2, b),
         nth1(2, X, b).
 
-%% sublist_n - Determinista
+% sublist_n
 
 test(sublist_n_1) :-
         sublist_n([a,b,c], 2, [a,b]), !.
@@ -37,10 +35,34 @@ test(sublist_n_3, [fail]) :-
 test(sublist_n_4) :-
         sublist_n([a,b,c], 0, []), !.
 
-%% sublist_n - No Determinista
-
 test(sublist_n_5) :-
         findall(X,  sublist_n([a,b,c], 2, X), Xs),
+        writeln(Xs),
         Xs = [[b,c], [a,b]].
+
+
+% palindromo
+
+test(palindromo0) :-
+        palindromo([]), !.
+
+test(palindromo1) :-
+        palindromo([1]), !.
+
+test(palindromo2) :-
+        palindromo([1,2,1]), !.
+
+test(palindromo3) :-
+        palindromo([1,2,4,5,2,1,2,5,4,2,1]), !.
+
+test(palindromo3) :-
+        palindromo([1,2,4,5,2,2,5,4,2,1]), !.
+
+test(palindromo4, [fail]) :-
+        palindromo([1,2]), !.
+
+test(palindromo5, [fail]) :-
+        palindromo([1,2,3]), !.
+
 
 :- end_tests(lists).
